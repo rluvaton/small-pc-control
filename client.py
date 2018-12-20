c4d50774bcb550f9458a2ffce301d4c56798f047
Raz Luvaton <rluvaton@gmail.com>
2018-12-20 18:11:19 +0200

    Fix client communication

--- client.py
+++ client.py
@@ -4,20 +4,33 @@ import socket
 target_ip = '127.0.0.1'
 target_port = 9999
 
-hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 80
-target = '{}.{}.{}'.format(hostname, sld, tld)
-
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the client
-# client.connect((target, port))
-client.connect((target_ip, target_port))
+# connect the server
+# server.connect((target, port))
+server.connect((target_ip, target_port))
 
-# send some data (in this case a HTTP GET request)
-client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
+message = '{} : {} {}'.format('Register', 'test', 'password')
 
-# receive the response data (4096 is recommended buffer size)
-response = client.recv(4096)
+while True:
 
-print response
+    # ask the server whether he wants to continue
+    message = raw_input("> ")
+    print 'send: {}'.format(message)
+
+    if not server:
+        print 'Server Disconnected'
+        break
+
+    # message sent to server
+    server.send(message)
+
+    # message received from server
+    data = server.recv(4096)
+
+    # print the received message
+    # here it would be a reverse of sent message
+    print 'Received: {}'.format(data)
+
+server.close()
